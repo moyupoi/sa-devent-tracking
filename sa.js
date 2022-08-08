abf6af1b11f9216a91e855d30db1eb8012f1ccff
墨鱼卷 <tuoxie18@gmail.com>
2022-08-08 15:28:57 +0800

    fix: 增加开关

--- sa.js
+++ sa.js
@@ -14,6 +14,12 @@ let initStart = true;
 // 存默认值
 let defaultInfoData = false;
 let localStorage = window.localStorage;
+// 埋点开关
+let isSaSwitch = true;
+//上报事件
+let reportEvent = [];
+//不上报事件
+let reportEvenEx = [];
 
 /**
   外部调用函数
@@ -24,8 +30,10 @@ let localStorage = window.localStorage;
 */
 
 // 埋点初始化函数
-export const $saInit = (url, interval = 10000) => {
-    if (url && url !== '' && initStart) {
+export const $saInit = (url, reportEventParame, reportEvenExParams, interval = 10000) => {
+    if (url && url !== '' && initStart && isSaSwitch) {
+        reportEvent = reportEventParame;
+        reportEvenEx = reportEvenExParams;
         // 防止重复调用
         initStart = false;
         // 赋值URL
@@ -36,7 +44,9 @@ export const $saInit = (url, interval = 10000) => {
             // 开始执行轮询
             initSetInterval(interval);
             // 监听错误日志
-            collectErrLogReport();
+            if (isAllowReporting('100098')) {
+                collectErrLogReport();
+            }
             // 获取系统信息
             getUA();
             moyuLog({
@@ -56,132 +66,148 @@ export const $setDefaultInfo = params =>
 
 // 外部调用
 export const $track = event => {
-    // 判断初始化默认数据中是否有值
-    if (defaultInfoData) {
-        const data = {
-            ...defaultInfoData,
-            ...event,
-            system: saSystem,
-            timestamp: new Date().getTime(),
-        };
-        $addDBbase(DATA_BASE_NAME, data).then(res => {
-            if (res) {
-                moyuLog({
-                    text: '埋点...事件埋点成功!',
-                    color: '#04c160',
-                    data,
-                });
-            } else {
-                moyuLog({
-                    text: '埋点...事件埋点失败...',
-                    color: '#d16770',
-                });
-            }
-        });
-    } else {
-        // 如果没有默认值，则把数据存到临时Storage中
-        setTemporaryStorage(event);
+    /**
+        isSaSwitch: 判断是否能上报事件
+        isAllowReporting: 判断是否允许上报
+    */
+    if (isSaSwitch && isAllowReporting(event.eventId)) {
+        // 判断初始化默认数据中是否有值
+        if (defaultInfoData) {
+            const data = {
+                ...defaultInfoData,
+                ...event,
+                system: saSystem,
+                timestamp: new Date().getTime(),
+            };
+            $addDBbase(DATA_BASE_NAME, data).then(res => {
+                if (res) {
+                    moyuLog({
+                        text: '埋点...事件埋点成功!',
+                        color: '#04c160',
+                        data,
+                    });
+                } else {
+                    moyuLog({
+                        text: '埋点...事件埋点失败...',
+                        color: '#d16770',
+                    });
+                }
+            });
+        } else {
+            // 如果没有默认值，则把数据存到临时Storage中
+            setTemporaryStorage(event);
+        }
     }
 };
 
 // 事件埋点: 页面加载的性能指标
 export const $initializationDetection = () => {
-    try {
-        let navigation = performance.getEntriesByType('navigation');
-        let memory = performance.memory;
-        let paint = performance.getEntriesByType('paint');
-        let firstPaintTime = 0;
-        let firstContentfulPaint = 0;
-        if (navigation && navigation.length > 0 && navigation[0].type && paint && paint.length > 0) {
-            navigation = navigation[0];
-            firstPaintTime = paint[0].startTime;
-            firstContentfulPaint = paint[1].startTime;
-        } else {
-            return;
-        }
-        const { domainLookupStart = 0, domainLookupEnd = 0, connectStart = 0, connectEnd = 0, responseStart = 0, responseEnd = 0, domComplete = 0, domInteractive = 0, domContentLoadedEventEnd = 0, domContentLoadedEventStart = 0, loadEventEnd = 0, fetchStart = 0, loadEventStart = 0, requestStart = 0, redirectEnd = 0, redirectStart = 0 } = navigation;
-        const { jsHeapSizeLimit = 0, totalJSHeapSize = 0, usedJSHeapSize = 0 } = memory;
-        const data = {
-            param: [
-                {
-                    name: 'DNS查询耗时',
-                    value: `${(domainLookupEnd - domainLookupStart).toFixed(0)}ms`,
-                },
-                {
-                    name: 'TCP链接耗时',
-                    value: `${(connectEnd - connectStart).toFixed(0)}ms`,
-                },
-                {
-                    name: '网络请求耗时(TTFB)',
-                    value: `${(responseStart - requestStart).toFixed(0)}ms`,
-                },
-                {
-                    name: 'request请求耗时',
-                    value: `${responseEnd === 0 ? 0 : (responseEnd - responseStart).toFixed(0)}ms`,
-                },
-                {
-                    name: '解析dom树耗时',
-                    value: `${(domInteractive - responseEnd).toFixed(0)}ms`,
-                },
-                {
-                    name: '白屏时间',
-                    value: `${(responseEnd - fetchStart).toFixed(0)}ms`,
-                },
-                {
-                    name: 'js脚本执行时间',
-                    value: `${(domContentLoadedEventEnd - domContentLoadedEventStart).toFixed(0)}ms`,
-                },
-                {
-                    name: '首屏渲染时间',
-                    value: `${firstPaintTime.toFixed(0)}ms`,
-                },
-                {
-                    name: '首次有内容渲染时间',
-                    value: `${firstContentfulPaint.toFixed(0)}ms`,
-                },
-                {
-                    name: '页面完全加载时间',
-                    value: `${(loadEventStart - fetchStart).toFixed(0)}ms`,
-                },
-                {
-                    name: '资源加载耗时',
-                    value: `${(loadEventStart - domContentLoadedEventEnd).toFixed(0)}ms`,
-                },
-                {
-                    name: '重定向次数',
-                    value: `${(performance.navigation.redirectCount).toFixed(0)}次`,
-                },
-                {
-                    name: '重定向耗时',
-                    value: `${(redirectEnd - redirectStart).toFixed(0)}ms`,
-                },
-                {
-                    name: '内存大小的限制',
-                    value: `${clip(jsHeapSizeLimit / 1024 / 1024)} MB`,
-                },
-                {
-                    name: '已分配大小',
-                    value: `${clip(totalJSHeapSize / 1024 / 1024)} MB`,
-                },
-                {
-                    name: '当前JS活跃内存大小',
-                    value: `${clip(usedJSHeapSize / 1024 / 1024)} MB`,
-                },
-            ]
-        };
+    const eventId = '100099';
+    if (isSaSwitch && isAllowReporting(eventId)) {
+        try {
+            let navigation = performance.getEntriesByType('navigation');
+            let memory = performance.memory;
+            let paint = performance.getEntriesByType('paint');
+            let firstPaintTime = 0;
+            let firstContentfulPaint = 0;
+            if (navigation && navigation.length > 0 && navigation[0].type && paint && paint.length > 0) {
+                navigation = navigation[0];
+                firstPaintTime = paint[0].startTime;
+                firstContentfulPaint = paint[1].startTime;
+            } else {
+                return;
+            }
+            const { domainLookupStart = 0, domainLookupEnd = 0, connectStart = 0, connectEnd = 0, responseStart = 0, responseEnd = 0, domComplete = 0, domInteractive = 0, domContentLoadedEventEnd = 0, domContentLoadedEventStart = 0, loadEventEnd = 0, fetchStart = 0, loadEventStart = 0, requestStart = 0, redirectEnd = 0, redirectStart = 0 } = navigation;
+            const { jsHeapSizeLimit = 0, totalJSHeapSize = 0, usedJSHeapSize = 0 } = memory;
+            const data = {
+                eventId,
+                param: [
+                    {
+                        name: 'DNS查询耗时',
+                        value: `${(domainLookupEnd - domainLookupStart).toFixed(0)}ms`,
+                    },
+                    {
+                        name: 'TCP链接耗时',
+                        value: `${(connectEnd - connectStart).toFixed(0)}ms`,
+                    },
+                    {
+                        name: '网络请求耗时(TTFB)',
+                        value: `${(responseStart - requestStart).toFixed(0)}ms`,
+                    },
+                    {
+                        name: 'request请求耗时',
+                        value: `${responseEnd === 0 ? 0 : (responseEnd - responseStart).toFixed(0)}ms`,
+                    },
+                    {
+                        name: '解析dom树耗时',
+                        value: `${(domInteractive - responseEnd).toFixed(0)}ms`,
+                    },
+                    {
+                        name: '白屏时间',
+                        value: `${(responseEnd - fetchStart).toFixed(0)}ms`,
+                    },
+                    {
+                        name: 'js脚本执行时间',
+                        value: `${(domContentLoadedEventEnd - domContentLoadedEventStart).toFixed(0)}ms`,
+                    },
+                    {
+                        name: '首屏渲染时间',
+                        value: `${firstPaintTime.toFixed(0)}ms`,
+                    },
+                    {
+                        name: '首次有内容渲染时间',
+                        value: `${firstContentfulPaint.toFixed(0)}ms`,
+                    },
+                    {
+                        name: '页面完全加载时间',
+                        value: `${(loadEventStart - fetchStart).toFixed(0)}ms`,
+                    },
+                    {
+                        name: '资源加载耗时',
+                        value: `${(loadEventStart - domContentLoadedEventEnd).toFixed(0)}ms`,
+                    },
+                    {
+                        name: '重定向次数',
+                        value: `${performance.navigation.redirectCount.toFixed(0)}次`,
+                    },
+                    {
+                        name: '重定向耗时',
+                        value: `${(redirectEnd - redirectStart).toFixed(0)}ms`,
+                    },
+                    {
+                        name: '内存大小的限制',
+                        value: `${clip(jsHeapSizeLimit / 1024 / 1024)} MB`,
+                    },
+                    {
+                        name: '已分配大小',
+                        value: `${clip(totalJSHeapSize / 1024 / 1024)} MB`,
+                    },
+                    {
+                        name: '当前JS活跃内存大小',
+                        value: `${clip(usedJSHeapSize / 1024 / 1024)} MB`,
+                    },
+                ],
+            };
 
-        $track(data);
-        moyuLog({
-            text: '埋点...首屏性能检测！',
-            color: '#04c160',
-            data,
-        });
-    } catch (e) {};
+            $track(data);
+            moyuLog({
+                text: '埋点...首屏性能检测！',
+                color: '#04c160',
+                data,
+            });
+        } catch (e) {}
+    }
 };
 
 export const $saReset = () => {
     initStart = false;
-}
+};
+
+// 埋点开关
+export const $saSwitch = params => {
+    isSaSwitch = params;
+    if (!isSaSwitch) localStorage.removeItem('saTempData');
+};
 
 // 轮询函数
 const initSetInterval = interval => {
@@ -338,12 +364,12 @@ const collectErrLogReport = () => {
                 fn = fn.caller;
             }
             errorText = ext.join(',');
-        };
+        }
         if (msg.indexOf("'")) {
-          msg = msg.replace(/'/g, '"');
+            msg = msg.replace(/'/g, '"');
         }
         if (errorText.indexOf("'")) {
-          errorText = errorText.replace(/'/g, '"');
+            errorText = errorText.replace(/'/g, '"');
         }
         const data = {
             eventId: '100098',
@@ -373,30 +399,46 @@ const collectErrLogReport = () => {
                     if (item.eventId === '100098' && item.param && item.param.msg === msg) {
                         // 重复了，并且更新
                         let frequency = item.frequency ? item.frequency + 1 : 1;
-                        $updateDBbase(DATA_BASE_NAME, {...item, frequency});
+                        $updateDBbase(DATA_BASE_NAME, { ...item, frequency });
                     } else {
                         // 不重复
                         isRepeat = true;
                     }
-                })
+                });
                 if (isRepeat) $track(data);
             } else {
-              $track(data);
+                $track(data);
             }
-        })
-    }
+        });
+    };
 };
 
 const clip = num => num.toString().replace(/(?<=\.\d{2}).*$/, '');
 
 // 获取系统信息
 const getUA = () => {
-  const uaParser = new UAParser(window.navigator.userAgent);
-  const result = uaParser.getResult();
-  const { browser, cpu, os } = result;
-  return {
-    cpu: cpu.architecture || '',
-    browser: `${browser.name} ${browser.version}`,
-    os: `${os.name} ${os.version}`,
-  };
+    const uaParser = new UAParser(window.navigator.userAgent);
+    const result = uaParser.getResult();
+    const { browser, cpu, os } = result;
+    return {
+        cpu: cpu.architecture || '',
+        browser: `${browser.name} ${browser.version}`,
+        os: `${os.name} ${os.version}`,
+    };
+};
+
+// 判断是否可以上报
+const isAllowReporting = eventId => {
+    let result = true;
+    // 允许上报事件
+    if (reportEvent && reportEvent.length > 0 && reportEvent.filter(item => item !== eventId).length > 0) {
+        result = false;
+    }
+
+    // 不允许上报事件
+    if (reportEvenEx && reportEvenEx.length > 0 && reportEvenEx.filter(item => item === eventId).length > 0) {
+        result = false;
+    }
+
+    return result;
 };
